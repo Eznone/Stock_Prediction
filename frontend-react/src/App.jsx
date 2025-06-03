c2ad2fa1f1fcc743d72260091c4a5dee20781f49
Enzo <enzotresmediano@gmail.com>
2025-06-03 10:09:48 -0300

    Updated Header

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './assets/css/style.css'
+import Header from './components/header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-
+    <Header />
+    <Main />
+    <Footer />
     </>
   )
 }
