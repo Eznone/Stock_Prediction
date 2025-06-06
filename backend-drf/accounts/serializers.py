466e20afb0b418cf9d3fca7a73ccc5fc36651c66
Enzo <enzotresmediano@gmail.com>
2025-06-06 11:19:26 -0300

    UserSerializer made

---
+++ backend-drf/accounts/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = {"username", "email", "first_name", "last_name", "password"}
+
+    def create(self, validated_data):
+        # User.objects.create = saves passord in plain text
+        # User.objects.create_user = saves password in hashed format
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
