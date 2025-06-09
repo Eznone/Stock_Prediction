6abfa9239f5e22af6261342d1796a87981502ea9
Enzo <enzotresmediano@gmail.com>
2025-06-09 10:30:29 -0300

    Created design for Registration page

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
   return (
     <>
     <div className='container'>
-        <div className="p-5 text-center bg-light-dark">
+        <div className="p-5 text-center bg-light-blue rounded shadow-lg">
             <h1 className="text-light text-accent">Stock Prediction Portal</h1>
             <p className='text-light text-offwhite lead'>
               This Stock Prediction Portal leverages historical market data, technical indicators and advanced machine learning algorithms to forecast short- and long-term price trends. With a Django-powered API for data processing and a React frontend for real-time visualization, it provides actionable insights to help you make smarter investment decisions.
--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div className="text-light text-accent">Register</div>
+    <div className='container'>
+        <div className="row justify-content-center">
+            <div className="col-md-6 bg-light-blue p-5 rounded shadow-lg">
+                <h3 className="text-light text-accent text-center mb-2">Create an Account</h3>
+                <form>
+                    <input type="text" className="form-control mb-2" placeholder="Username"/>
+                    <input type="email" className="form-control mb-2" placeholder="Email address"/>
+                    <input type="password" className="form-control mb-4" placeholder="Set password"/>
+                    <button type="submit" className="btn btn-fill-ebffd8 d-block mx-auto">Register</button>
+                </form>
+            </div>    
+        </div>
+
+    </div>
   )
 }
 
